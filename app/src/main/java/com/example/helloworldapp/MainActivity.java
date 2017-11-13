425a6550a03cff58bd3587ee44f622b3f40c7c1e
Leonard Follner <Leonard.Follner@gmail.com>
2017-11-13 16:25:27 +0100

    initial commit

---
+++ app/src/main/java/com/example/helloworldapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.helloworldapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onTap(View view) {
+        EditText inputField = findViewById(R.id.input);
+        String inputMessage = inputField.getText().toString();
+    }
+}
